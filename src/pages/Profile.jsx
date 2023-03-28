b39ca953619c9af871b85ad3ef3591635a016e99
Kalyan Jana <79560709+KalyanJana@users.noreply.github.com>
2023-03-28 18:58:18 +0530

    authentication added
    added 'Please login' text if user is not logged in.

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -7,13 +7,17 @@ const Profile = () => {
   const {isAuthenticated, loading, user} = useContext(Context)
 
   return (
-    loading ? <Loader /> : (
-      <div>
-        <h1>{user?.name}</h1>
-        <p>{user?.email}</p>
-      </div>
+    isAuthenticated ? (
+      loading ? <Loader /> : (
+        <div>
+          <h1>{user?.name}</h1>
+          <p>{user?.email}</p>
+        </div>
+      )
+    ):(
+      <p >Please login..</p>
     )
-    
+  
   )
 }
 
